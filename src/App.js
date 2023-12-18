9acab9f2eb254bd7a0d4569ecd55365ef6f7dea6
Dadi B <benmezianedadi@yahoo.fr>
2023-12-18 23:45:50 +0100

    création de Error

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./sass/index.scss";
 import Home from "./pages/Home/Home";
+import Error from "./pages/Error/Error";
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
@@ -12,6 +13,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />} /> 
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/pages/Error/Error.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Error = () => {
+    return (
+
+        <div className="error">
+            <main>
+                <div className="error_nbr">404</div>
+                
+                <div className="error_text">Oups! La page que vous demandez n'existe pas</div>
+            </main>
+        </div>
+    );
+};
+
+export default Error;
