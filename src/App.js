b360c7c1da4cf6a4f337a9374c2660c7485e6f74
Dadi B <benmezianedadi@yahoo.fr>
2023-12-18 21:08:49 +0100

    creation de Banner

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./sass/index.scss";
 import Header from "./components/Header/Header";
+import Banner from "./components/Banner/Banner";
 import Footer from "./components/Footer/Footer";
+
 
 
 
@@ -9,7 +11,7 @@ const App = () => {
   return (
     <main>
       <Header/>
-
+      <Banner/>
       <Footer/>
     </main>
   );
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+function Banner({ image, texte }) {
+  return (
+    <div className="banner">
+      <img className="banner_img" src={image} alt="" />
+      <div className="banner_sombre"></div>
+      <span className="banner_txt">{texte}</span>
+    </div>
+  );
+}
+
+export default Banner;
+
