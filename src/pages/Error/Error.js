80cfe1dbd3a46c2b8218662b0c00ff9da9e372c1
Dadi B <benmezianedadi@yahoo.fr>
2024-01-02 15:06:38 +0100

    Ajout du composant Error

--- src/pages/Error/Error.js
+++ src/pages/Error/Error.js
@@ -1,21 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Error from "../../components/Error/Error";
 
-
-const Error = () => {
-    return (
-
-        <div className="error">
-
-            <p className="error_nbr">404</p>
-
-            <p className="error_text">Oups! La page que vous demandez n'existe pas</p>
-            <Link to="/" className="error_Link">
-                Retourner sur la page d'accueil
-            </Link>
-
-        </div>
-    );
+const NoFound = () => {
+  return (
+    <main>
+    <Error />
+  </main>     
+  );
 };
-
-export default Error;
+export default NoFound;
